18885fecc82409aaa3432f32df643d1dbdb85a6f
Gen <archiherewego@gmail.com>
2021-03-09 18:24:35 +0900

    Update UI
    CircularProgressIndicatorを使うとこういうことができる
    というようなコンポーネントの一覧とか、Modifierの使い方のサンプルとか、どうやったら参照できるんだ

--- app/src/main/java/com/example/androiddevchallenge/ui/timer/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/timer/TimerScreen.kt
@@ -1,12 +1,16 @@
 package com.example.androiddevchallenge.ui.timer
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.MainViewModel
 import com.example.androiddevchallenge.TimerState
 
@@ -14,13 +18,20 @@ import com.example.androiddevchallenge.TimerState
 fun TimerScreen(vm: MainViewModel) {
     val displayText: String by vm.displayString.collectAsState(initial = "")
     val remainRatio: Float by vm.remainRatio.collectAsState(initial = 1f)
-    Column() {
-        Timer(displayText, remainRatio)
+    Column(Modifier.fillMaxWidth()) {
+        Timer(
+                displayText = displayText,
+                remainRatio = remainRatio,
+        )
+        val modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 32.dp, vertical = 18.dp)
         when (vm.timerState.value) {
-            TimerState.START -> Button(onClick = vm::onStop) {
+            TimerState.START -> Button(onClick = vm::onStop, modifier = modifier) {
                 Text(text = "STOP")
             }
-            TimerState.STOP -> Button(onClick = vm::onStart) {
+            TimerState.STOP -> Button(onClick = vm::onStart, modifier = modifier) {
                 Text(text = "START")
             }
             else -> TODO("${vm.timerState.value} is not implemented")
@@ -29,9 +40,22 @@ fun TimerScreen(vm: MainViewModel) {
 }
 
 @Composable
-fun Timer(displayText: String, remainRatio: Float = 1f) {
-    Text(text = displayText)
-    Text(text = "remain ratio = $remainRatio")
+fun Timer(
+        modifier: Modifier = Modifier,
+        displayText: String,
+        remainRatio: Float = 1f,
+) {
+    Column(
+            modifier = modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = displayText)
+        Text(text = "remain ratio = $remainRatio")
+        CircularProgressIndicator(progress = remainRatio)
+    }
 }
 
 @Composable
